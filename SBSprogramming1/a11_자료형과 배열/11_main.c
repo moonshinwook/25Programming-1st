c6893e4232a7282b7fcaef10b0ae889689393e8b
moonshinwook <msw4761@naver.com>
2025-10-29 22:31:13 +0900

    (2025-10-29) 배열과 문자에 대한 이해

--- SBSprogramming1/a11_자료형과 배열/11_main.c
+++ SBSprogramming1/a11_자료형과 배열/11_main.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdbool.h>
 
 // 어떤 점이 장점이 되는가? 
 // 기존 방식, 새로운 방식 둘다 사용. 
@@ -12,7 +13,25 @@
 // 함수 헤더() => {	};
 // 자료형 int x 100개 크기 4바이트 정수 표현 {		} 
 
-int main()
+bool IsStrCompare()
+{
+	char name[10];
+	printf("가위를 입력하세요 : %s");
+	scanf("%s", name);
+
+	if (strcmp(name, "가위") == 0)
+	{
+		printf("같다\n");
+		return true;
+	}
+	else
+	{
+		printf("다르다\n");
+		return false;
+	}
+}
+
+int main(void)
 {
 	// 반복적으로 사용되는 자료형을 배열로 표현한다. 코드를 개선하기 위함.
 	// 배열 사용(1)
@@ -29,8 +48,8 @@ int main()
 		printf("%d번째값 : %d\n", i, numArray[i]);
 	}
 	// 배열 사용(2)
-	
-	int numArray2[100] = {0, 1, 2, 3 }; // int형 자료형이 100개 저장될 수 있는 공간을 선언한다. int형 배열의 크기가 100인 녀석이다.
+
+	int numArray2[100]; // int형 자료형이 100개 저장될 수 있는 공간을 선언한다. int형 배열의 크기가 100인 녀석이다.
 	int numArray3[40] = { 0, 1, 2, 3 };
 	// sizeof(변수의 이름) => 바이트의 크기를 반환해줍니다.
 
@@ -68,18 +87,22 @@ int main()
 	// 왜 문자의 끝을 표현을 해줘야 하는가? <- 
 
 	// 반복문을 사용해서 문자가 끝이 났는지 확인하는 코드를 작성해봅시다.
-	
+
 	char str[50];
 	int index = 0;
-	//printf("문자열 입력 : ");
+	printf("문자열 입력 : ");
 	scanf("%s", str); // &를 쓰면 안된다! 왜? 주소 연산자는 변수를 주소로 바꿔주는 것임. str은 주소를 표현하는 것임.
 
+	// printf("안녕하세요"); 이 코드에는 밑의 while 반복문이 실행되고 있는 것이다.
 
-	while (str[index] != '\0') //str들어있는 문자를 모두 출력해라.
+	while (str[index] != '\0') // str들어있는 문자를 모두 출력해라.
 	{
-		printf("%c", str[index]);
+		printf("%c\n", str[index]);
 		index++;
 	}
+	
+
+
 	// char arr1[] = {'H', 'I'} => 문자의 배열
 	// char arr2[] \ {'H', 'I', '\0'}; => 문자열
 
@@ -90,25 +113,31 @@ int main()
 	// 문자의 입력을 받아서 비교를 할겁니다.
 	// 리터럴문자 저장되는 영역 따로 존재한다.
 
-	char name[10]; //
+	//char name[10];
 
-	printf("리터럴문자의 주소 : %x", "안녕하세요");
-	scanf("%s", name); //name의 주소
+	//printf("리터럴문자의 주소 : %x", "안녕하세요");
+	//scanf("%s", name); //name의 주소
 
-	// 문자열 비교		-> name 10개의 char데이터를 저장할 수 있는 영역을 생성.
+	//// 문자열 비교		-> name 10개의 char데이터를 저장할 수 있는 영역을 생성.
 
-	if (name == "가위")
-	{
-		printf("같다\n");
-	}
-	else 
-	{
-		printf("다르다\n");
-	}
+	//if (name == "가위")
+	//{
+	//	printf("같다\n");
+	//}
+	//else
+	//{
+	//	printf("다르다\n");
+	//}
 
 	// name "리터럴문자" 주소가 다르다. 저장되는 영역이 다르기 때문.
 	// 문자열의 비교 함수로 만들어본다. 실행할 수 있게 본다.
 
-	// name 가위 들어있는 내용은 같다. '가' '위' '\0' => 같다 알려주는 함수  bool IsStrCompare
+	// name 가위 들어있는 내용은 같다. '가' '위' '\0' => 같다 알려주는 함수  bool  IsStrCompare
 	// 주소의 개념이 있기때문에 어려운 내용임. -> 겜프2에서 포인터에서 다룰 영역이기 때문에 지금 개념을 정리해둬야 한다.
+	
+	IsStrCompare();
+	return 0;
+
+
+
 }
